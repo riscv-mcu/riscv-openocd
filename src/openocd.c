796b53d7502d8a3c2d4bb7d5219d57f23dc63dd6
wangyanwen <wangyanwen@nucleisys.com>
2023-05-16 15:21:24 +0800

    Add init resethalt command
    Change-Id: Ibe8359e0db760d88745073a4834c03215e4d8ad0

--- src/openocd.c
+++ src/openocd.c
@@ -108,9 +108,16 @@ COMMAND_HANDLER(handle_noinit_command)
 /* OpenOCD can't really handle failure of this command. Patches welcome! :-) */
 COMMAND_HANDLER(handle_init_command)
 {
+	bool resethalt = false;
 
 	if (CMD_ARGC != 0)
 		return ERROR_COMMAND_SYNTAX_ERROR;
+
+	if (CMD_ARGC == 1) {
+		if(0 == strcmp("resethalt", CMD_ARGV[0])) {
+			resethalt = true;
+		}
+	}
 
 	int retval;
 	static int initialized;
@@ -121,7 +128,11 @@ COMMAND_HANDLER(handle_init_command)
 
 	bool save_poll_mask = jtag_poll_mask();
 
-	retval = command_run_line(CMD_CTX, "target init");
+	if (resethalt) {
+		retval = command_run_line(CMD_CTX, "target init resethalt");
+	} else {
+		retval = command_run_line(CMD_CTX, "target init");
+	}
 	if (retval != ERROR_OK)
 		return ERROR_FAIL;
 
@@ -213,7 +224,7 @@ static const struct command_registration openocd_command_handlers[] = {
 			"Changes command mode from CONFIG to EXEC.  "
 			"Unless 'noinit' is called, this command is "
 			"called automatically at the end of startup.",
-		.usage = ""
+		.usage = "[resethalt]"
 	},
 	{
 		.name = "add_script_search_dir",
